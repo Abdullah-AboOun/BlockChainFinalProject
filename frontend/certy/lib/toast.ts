1285975c403f27c93559ffcc0248a95578e63d52
Abdullah <abdullah.h.pro@gmail.com>
2025-10-18 09:57:55 +0300

    Remove ignored files from repo

--- frontend/certy/lib/toast.ts
+++
@@ -1,9 +0,0 @@
-// Simple toast notification utility
-export const toast = {
-  success: (message: string) => {
-    console.log("✓", message)
-  },
-  error: (message: string) => {
-    console.error("✗", message)
-  },
-}
--- hardhat/ignition/modules/CertificateRegistry.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const CertificateModule = buildModule("CertificateModule", (m) => {
-  const certificateRegistry = m.contract("CertificateRegistry");
-
-  return { certificateRegistry };
-});
-
-export default CertificateModule;
